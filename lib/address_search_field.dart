9648e7fe2e952ab650c10c117e0ac7cd47732db4
Jose Luna <39394112+JosLuna98@users.noreply.github.com>
2020-11-27 16:38:35 -0500

    new file export

--- lib/address_search_field.dart
+++ lib/address_search_field.dart
@@ -2,11 +2,23 @@
 /// It can be used to get Directions beetwen two points.
 library address_search_field;
 
-export 'package:address_search_field/src/widgets/address_field.dart';
-export 'package:address_search_field/src/widgets/address_dialog.dart';
-export 'package:address_search_field/src/widgets/route_search_box.dart';
+import 'dart:async';
+import 'package:address_search_field/address_search_field.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:address_search_field/src/services/geo_methods.dart';
+import 'package:address_search_field/src/models/coords.dart';
+import 'package:address_search_field/src/models/address.dart';
+import 'package:address_search_field/src/models/directions.dart';
+
 export 'package:address_search_field/src/services/geo_methods.dart';
 export 'package:address_search_field/src/models/directions.dart';
 export 'package:address_search_field/src/models/address.dart';
 export 'package:address_search_field/src/models/bounds.dart';
 export 'package:address_search_field/src/models/coords.dart';
+
+part 'package:address_search_field/src/widgets/route_search_box.dart';
+part 'package:address_search_field/src/widgets/address_search_builder.dart';
+part 'package:address_search_field/src/widgets/address_search_dialog.dart';
+part 'package:address_search_field/src/models/addr_comm.dart';
