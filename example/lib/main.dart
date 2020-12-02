05f1b8abdc258019aa0d5dcab6cd0d6a61efba4c
Jose Luna <39394112+JosLuna98@users.noreply.github.com>
2020-12-02 05:45:35 -0500

    relocate function added and code improves

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -54,139 +54,134 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Plugin example app'),
-          ),
-          body: SingleChildScrollView(
-            physics: NeverScrollableScrollPhysics(),
-            child: Column(
-              children: [
-                RouteSearchBox(
-                  geoMethods: geoMethods,
-                  originIsMyLocation: true,
-                  originCtrlr: origCtrl,
-                  destinationCtrlr: destCtrl,
-                  builder: (context, originBuilder, destinationBuilder,
-                      waypointBuilder,
-                      {getDirections, waypointsMgr}) {
-                    return Container(
-                      padding: EdgeInsets.symmetric(horizontal: 15.0),
-                      color: Colors.green[50],
-                      height: 150.0,
-                      child: Column(
-                        children: [
-                          TextField(
-                            controller: origCtrl,
-                            onTap: () => showDialog(
-                              context: context,
-                              builder:
-                                  (context) => // You can build your own widget to search addresses.
-                                      originBuilder.build(
-                                (context, snapshot,
-                                        {controller,
-                                        getGeometry,
-                                        searchAddress}) =>
-                                    AddressSearchDialog(
-                                  snapshot: snapshot,
-                                  controller: controller,
-                                  searchAddress: searchAddress,
-                                  getGeometry: getGeometry,
-                                  onDone: (address) => null,
-                                ),
-                              ),
-                            ),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text('Plugin example app'),
+      ),
+      body: Column(
+        children: [
+          RouteSearchBox(
+            geoMethods: geoMethods,
+            originIsMyLocation: true,
+            originCtrl: origCtrl,
+            destinationCtrl: destCtrl,
+            builder: (context, originBuilder, destinationBuilder,
+                {waypointBuilder, getDirections, relocate, waypointsMgr}) {
+              return Container(
+                padding: EdgeInsets.symmetric(horizontal: 15.0),
+                color: Colors.green[50],
+                height: 150.0,
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: origCtrl,
+                      onTap: () => showDialog(
+                        context: context,
+                        builder:
+                            (context) => // You can build your own widget to search addresses.
+                                originBuilder.build(
+                          (context, snapshot,
+                                  {controller, getGeometry, searchAddress}) =>
+                              // It's just an example, you should use `buildDefault` to use this widget.
+                              AddressSearchDialog(
+                            snapshot: snapshot,
+                            controller: controller,
+                            searchAddress: searchAddress,
+                            getGeometry: getGeometry,
+                            onDone: (address) => null,
                           ),
-                          TextField(
-                            controller: destCtrl,
-                            onTap: () => showDialog(
-                              context: context,
-                              builder:
-                                  (context) => // You can build the default widget in the plugin to search addresses.
-                                      destinationBuilder.buildDefault(
-                                builder: AddressDialogBuilder(),
-                                onDone: (address) => null,
-                              ),
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              ElevatedButton(
-                                child: Text('Waypoints'),
-                                onPressed: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      fullscreenDialog: true,
-                                      builder: (context) => Waypoints(
-                                          waypointsMgr, waypointBuilder),
-                                    )),
-                              ),
-                              ElevatedButton(
-                                child: Text('Search'),
-                                onPressed: () async {
-                                  try {
-                                    final result = await getDirections();
-                                    markers.clear();
-                                    polylines.clear();
-                                    markers.addAll([
-                                      Marker(
-                                          markerId: MarkerId('origin'),
-                                          position: result.origin.coords),
-                                      Marker(
-                                          markerId: MarkerId('dest'),
-                                          position: result.destination.coords)
-                                    ]);
-                                    result.waypoints.asMap().forEach(
-                                        (key, value) => markers.add(Marker(
-                                            markerId: MarkerId('point$key'),
-                                            position: value.coords)));
-                                    polylines.add(Polyline(
-                                      polylineId: PolylineId('result'),
-                                      points: result.points,
-                                      color: Colors.blue[400],
-                                      width: 5,
-                                    ));
-                                    setState(() {});
-                                    await _controller.animateCamera(
-                                        CameraUpdate.newLatLngBounds(
-                                            result.bounds, 60.0));
-                                  } catch (e) {
-                                    print(e);
-                                  }
-                                },
-                              ),
-                            ],
-                          ),
-                        ],
+                        ),
                       ),
-                    );
-                  },
-                ),
-                Container(
-                  height: constraints.constrainHeight() - 205.0,
-                  child: GoogleMap(
-                    compassEnabled: true,
-                    myLocationEnabled: true,
-                    myLocationButtonEnabled: true,
-                    rotateGesturesEnabled: true,
-                    initialCameraPosition: CameraPosition(
-                      target: _initialPositon,
-                      zoom: 14.5,
                     ),
-                    onMapCreated: (GoogleMapController controller) =>
-                        _controller = controller,
-                    polylines: polylines,
-                    markers: markers,
-                  ),
+                    TextField(
+                      controller: destCtrl,
+                      onTap: () => showDialog(
+                        context: context,
+                        builder:
+                            (context) => // You can build the default widget in the plugin to search addresses.
+                                destinationBuilder.buildDefault(
+                          builder: AddressDialogBuilder(),
+                          onDone: (address) => null,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          child: Text('Points'),
+                          onPressed: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                fullscreenDialog: true,
+                                builder: (context) =>
+                                    Waypoints(waypointsMgr, waypointBuilder),
+                              )),
+                        ),
+                        ElevatedButton(
+                          child: Text('Relocate'),
+                          onPressed: relocate,
+                        ),
+                        ElevatedButton(
+                          child: Text('Search'),
+                          onPressed: () async {
+                            try {
+                              final result = await getDirections();
+                              markers.clear();
+                              polylines.clear();
+                              markers.addAll([
+                                Marker(
+                                    markerId: MarkerId('origin'),
+                                    position: result.origin.coords),
+                                Marker(
+                                    markerId: MarkerId('dest'),
+                                    position: result.destination.coords)
+                              ]);
+                              result.waypoints.asMap().forEach((key, value) =>
+                                  markers.add(Marker(
+                                      markerId: MarkerId('point$key'),
+                                      position: value.coords)));
+                              polylines.add(Polyline(
+                                polylineId: PolylineId('result'),
+                                points: result.points,
+                                color: Colors.blue[400],
+                                width: 5,
+                              ));
+                              setState(() {});
+                              await _controller.animateCamera(
+                                  CameraUpdate.newLatLngBounds(
+                                      result.bounds, 60.0));
+                            } catch (e) {
+                              print(e);
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
+              );
+            },
+          ),
+          Expanded(
+            child: GoogleMap(
+              compassEnabled: true,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              rotateGesturesEnabled: true,
+              initialCameraPosition: CameraPosition(
+                target: _initialPositon,
+                zoom: 14.5,
+              ),
+              onMapCreated: (GoogleMapController controller) =>
+                  _controller = controller,
+              polylines: polylines,
+              markers: markers,
             ),
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
--- lib/src/widgets/route_search_box.dart
+++ lib/src/widgets/route_search_box.dart
@@ -6,18 +6,20 @@ class RouteSearchBox extends StatefulWidget {
   RouteSearchBox({
     this.originIsMyLocation = false,
     this.onOriginLoading = 'Loading..',
-    this.onOriginError,
+    this.onOriginError = 'Unidentified place',
     @required this.geoMethods,
-    TextEditingController originCtrlr,
-    TextEditingController destinationCtrlr,
-    TextEditingController waypointCtrlr,
+    TextEditingController originCtrl,
+    TextEditingController destinationCtrl,
+    TextEditingController waypointCtrl,
     @required this.builder,
   })  : assert(originIsMyLocation != null),
+        assert(onOriginLoading != null),
+        assert(onOriginError != null),
         assert(geoMethods != null),
         assert(builder != null),
-        this.originCtrlr = originCtrlr ?? TextEditingController(),
-        this.destinationCtrlr = destinationCtrlr ?? TextEditingController(),
-        this.waypointCtrlr = waypointCtrlr ?? TextEditingController(),
+        this.originCtrl = originCtrl ?? TextEditingController(),
+        this.destinationCtrl = destinationCtrl ?? TextEditingController(),
+        this.waypointCtrl = waypointCtrl ?? TextEditingController(),
         super();
 
   /// If it's true the user location will be set as the origin [Address].
@@ -33,21 +35,22 @@ class RouteSearchBox extends StatefulWidget {
   final GeoMethods geoMethods;
 
   /// controller for text used to search an [Address].
-  final TextEditingController originCtrlr;
+  final TextEditingController originCtrl;
 
   /// controller for text used to search an [Address].
-  final TextEditingController destinationCtrlr;
+  final TextEditingController destinationCtrl;
 
   /// controller for text used to search an [Address].
-  final TextEditingController waypointCtrlr;
+  final TextEditingController waypointCtrl;
 
   /// Custom [WidgetBuilder] that builds a widget by two [AddressSearchField] to get two [Address] objects and be able to call Google Directions API by `getDirections` to finally get a [Directions] object.
   final Widget Function(
     BuildContext context,
     AddressSearchBuilder originBuilder,
-    AddressSearchBuilder destinationBuilder,
-    AddressSearchBuilder waypointBuilder, {
+    AddressSearchBuilder destinationBuilder, {
+    AddressSearchBuilder waypointBuilder,
     WaypointsManager waypointsMgr,
+    void Function() relocate,
     Future<Directions> Function() getDirections,
   }) builder;
 
@@ -59,16 +62,19 @@ class _RouteSearchBoxState extends State<RouteSearchBox> {
   /// Permits to work with the found [Address] by a [RouteSearchBox].
   final _addrComm = _AddrComm();
 
+  /// Sets if [_setOrigin] will over-write [widget.originCtrlr.text].
+  bool _forced = false;
+
   @override
   Widget build(BuildContext context) {
     if (widget.originIsMyLocation) {
       return FutureBuilder(
         future: _setOrigin(),
         builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          widget.originCtrlr.text = (snapshot.hasError)
-              ? widget.onOriginError ?? snapshot.error
+          widget.originCtrl.text = (snapshot.hasError)
+              ? widget.onOriginError
               : (snapshot.connectionState != ConnectionState.done)
-                  ? widget.onOriginLoading ?? ''
+                  ? widget.onOriginLoading
                   : snapshot.data;
           return _widget(context);
         },
@@ -82,64 +88,74 @@ class _RouteSearchBoxState extends State<RouteSearchBox> {
         context,
         AddressSearchBuilder._fromBox(
           widget.geoMethods,
-          widget.originCtrlr,
+          widget.originCtrl,
           _BoxId.origin,
           _addrComm,
         ),
         AddressSearchBuilder._fromBox(
           widget.geoMethods,
-          widget.destinationCtrlr,
+          widget.destinationCtrl,
           _BoxId.destination,
           _addrComm,
         ),
-        AddressSearchBuilder._fromBox(
+        waypointBuilder: AddressSearchBuilder._fromBox(
           widget.geoMethods,
-          widget.waypointCtrlr,
+          widget.waypointCtrl,
           _BoxId.waypoints,
           _addrComm,
         ),
         waypointsMgr: _addrComm.readAddrList(_BoxId.waypoints),
+        relocate: _relocate,
         getDirections: _getDirections,
       );
 
   /// When [widget.originIsMyLocation] is true, it will set current location in origin field.
   Future<String> _setOrigin() async {
-    if (_addrComm.readAddr(_BoxId.origin).hasReference)
+    if (_addrComm.readAddr(_BoxId.origin).hasReference && !_forced)
       return Future.value(_addrComm.readAddr(_BoxId.origin).reference);
-    final Location location = Location();
-    if (!await location.serviceEnabled()) {
-      if (!await location.requestService()) throw 'GPS service is disabled';
+    try {
+      final Location location = Location();
+      if (!await location.serviceEnabled()) {
+        if (!await location.requestService()) throw RouteError.gps_disabled;
+      }
+      if (await location.hasPermission() == PermissionStatus.denied) {
+        if (await location.requestPermission() != PermissionStatus.granted)
+          throw RouteError.no_gps_permission;
+      }
+      final locationData = await location.getLocation();
+      final coords = Coords(locationData.latitude, locationData.longitude);
+      Address address = await widget.geoMethods.geoLocatePlace(coords: coords);
+      if (address == null) {
+        _addrComm.writeAddr(_BoxId.origin, Address(coords: coords));
+        throw RouteError.location_not_found;
+      }
+      _addrComm.writeAddr(_BoxId.origin, address);
+      return Future.value(address.reference);
+    } catch (e) {
+      print(e);
+      throw e;
+    } finally {
+      _forced = false;
     }
-    if (await location.hasPermission() == PermissionStatus.denied) {
-      if (await location.requestPermission() != PermissionStatus.granted)
-        throw 'No GPS permissions';
-    }
-    final locationData = await location.getLocation();
-    final coords = Coords(locationData.latitude, locationData.longitude);
-    Address address = await widget.geoMethods.geoLocatePlace(coords: coords);
-    if (address == null) {
-      _addrComm.writeAddr(_BoxId.origin, Address(coords: coords));
-      throw 'Location not found';
-    }
-    _addrComm.writeAddr(_BoxId.origin, address);
-    return Future.value(address.reference);
+  }
+
+  /// Rebuilds widget and set origin again.
+  void _relocate() {
+    setState(() => _forced = true);
   }
 
   /// Gets directions using all the [Address] objects in [_addrComm].
   Future<Directions> _getDirections() async {
-    if (!_addrComm.readAddr(_BoxId.origin).hasCoords) throw 'No origin coords';
+    if (!_addrComm.readAddr(_BoxId.origin).hasCoords)
+      throw RouteError.no_origin_coords;
     if (!_addrComm.readAddr(_BoxId.destination).hasCoords)
-      throw 'No destination coords';
-    if (_addrComm.readAddr(_BoxId.origin).hasCoords &&
-        _addrComm.readAddr(_BoxId.destination).hasCoords) {
-      Directions direc = await widget.geoMethods.getDirections(
-          origin: _addrComm.readAddr(_BoxId.origin),
-          destination: _addrComm.readAddr(_BoxId.destination),
-          waypoints:
-              _addrComm.readAddrList(_BoxId.waypoints).valueNotifier.value);
-      if (direc == null) throw 'Directions not found';
-      return direc;
-    }
-    return null;
+      throw RouteError.no_dest_coords;
+    Directions direc = await widget.geoMethods.getDirections(
+        origin: _addrComm.readAddr(_BoxId.origin),
+        destination: _addrComm.readAddr(_BoxId.destination),
+        waypoints:
+            _addrComm.readAddrList(_BoxId.waypoints).valueNotifier.value);
+    if (direc == null) throw RouteError.directions_not_found;
+    return direc;
   }
 }
