bbdc9b304b79095bb843b4446ad75d8946b655fa
Jose Luna <39394112+JosLuna98@users.noreply.github.com>
2020-12-04 19:23:25 -0500

    location plugin removed

--- lib/address_search_field.dart
+++ lib/address_search_field.dart
@@ -4,7 +4,6 @@ library address_search_field;
 
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
 import 'package:address_search_field/src/utils/route_error.dart';
 import 'package:address_search_field/src/services/geo_methods.dart';
 import 'package:address_search_field/src/models/coords.dart';
@@ -21,6 +20,7 @@ export 'package:address_search_field/src/models/coords.dart';
 part 'package:address_search_field/src/widgets/route_search_box.dart';
 part 'package:address_search_field/src/widgets/address_search_builder.dart';
 part 'package:address_search_field/src/widgets/address_search_dialog.dart';
+part 'package:address_search_field/src/widgets/address_locator.dart';
 part 'package:address_search_field/src/models/addr_comm.dart';
 part 'package:address_search_field/src/models/waypoints_manager.dart';
 part 'package:address_search_field/src/utils/address_id.dart';
