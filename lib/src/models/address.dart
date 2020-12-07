ae014913d7efb5b6062f5516dcb19c1cef370ee8
Jose Luna <39394112+JosLuna98@users.noreply.github.com>
2020-12-06 22:02:19 -0500

    update function becomes in copyWith

--- lib/src/models/address.dart
+++ lib/src/models/address.dart
@@ -5,73 +5,64 @@ import 'package:address_search_field/src/models/coords.dart';
 /// Primary data of an address to perform geolocation processes.
 class Address {
   /// Coordinates.
-  Coords _coords;
+  final Coords coords;
 
   /// Bounds.
-  Bounds _bounds;
+  final Bounds bounds;
 
   /// Reference or streets.
-  String _reference;
+  final String reference;
 
   /// Place Id from Google Place API.
-  String _placeId;
+  final String placeId;
 
   /// Constructor for [Address].
   Address({Coords coords, Bounds bounds, String reference, String placeId})
-      : _coords = coords,
-        _bounds = bounds,
-        _reference = reference,
-        _placeId = placeId;
-
-  /// Address coordinates.
-  Coords get coords => _coords;
-
-  /// Address bounds.
-  Bounds get bounds => _bounds;
-
-  /// Address reference or streets.
-  String get reference => _reference;
-
-  /// Place Id to work with Google Place API
-  String get placeId => _placeId;
+      : this.coords = coords,
+        this.bounds = bounds,
+        this.reference = reference,
+        this.placeId = placeId;
 
   /// Checks if reference exists.
-  bool get hasReference => _reference != null && _reference.isNotEmpty;
+  bool get hasReference => reference != null && reference.isNotEmpty;
 
   /// Checks if Place Id exists.
-  bool get hasPlaceId => _placeId != null && _placeId.isNotEmpty;
+  bool get hasPlaceId => placeId != null && placeId.isNotEmpty;
 
   /// Checks if coords exists.
-  bool get hasCoords => _coords != null;
+  bool get hasCoords => coords != null;
 
   /// Checks if bounds exists.
-  bool get hasBounds => _bounds != null;
+  bool get hasBounds => bounds != null;
 
   /// Checks if all properties exist.
   bool get isCompleted => hasCoords && hasPlaceId && hasCoords && hasBounds;
 
-  /// Permits to update object by other object.
-  void update(Address newAddress) {
-    _coords = newAddress.coords;
-    _bounds = newAddress.bounds;
-    _reference = newAddress.reference;
-    _placeId = newAddress.placeId;
+  /// Permits to get an [Address] copy and update its data with other [Address].
+  Address copyWith(Address newAddress) {
+    assert(newAddress != null);
+    return Address(
+      coords: newAddress.coords ?? this.coords,
+      bounds: newAddress.bounds ?? this.bounds,
+      reference: newAddress.reference ?? this.reference,
+      placeId: newAddress.placeId ?? this.placeId,
+    );
   }
 
   @override
   String toString() =>
-      "${(hasPlaceId) ? _placeId : null} => $_reference\ncoords: [${_coords.toString()}]${(hasBounds) ? '\n${_bounds.toString()}' : ''}";
+      "${(hasPlaceId) ? placeId : null} => $reference\ncoords: [${coords.toString()}]${(hasBounds) ? '\n${bounds.toString()}' : ''}";
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
     return other is Address &&
-        other._coords == _coords &&
-        other._bounds == _bounds &&
-        other._reference == _reference &&
-        other._placeId == _placeId;
+        other.coords == coords &&
+        other.bounds == bounds &&
+        other.reference == reference &&
+        other.placeId == placeId;
   }
 
   @override
-  int get hashCode => hashValues(_coords, _bounds, _reference, _placeId);
+  int get hashCode => hashValues(coords, bounds, reference, placeId);
 }
